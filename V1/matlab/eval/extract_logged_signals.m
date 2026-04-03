a5766d6ea113f78ec687450ae664ecd6064d36cf
Piyush <pandapiyush086@gmail.com>
2026-04-03 14:31:17 +0530

    Plot extensions

--- V1/matlab/eval/extract_logged_signals.m
+++ V1/matlab/eval/extract_logged_signals.m
@@ -1,16 +1,16 @@
 function sig = extract_logged_signals(simOut)
-% Extract logged signals from Simulink.SimulationOutput
-
 logs = simOut.logsout;
 
 sig = struct();
 sig.t = simOut.tout;
 
-sig.r = logs.get("Desired Response").Values.Data;
-sig.d = logs.get("Disturbance").Values.Data;
-sig.u = logs.get("Controller Out").Values.Data;
-sig.y = logs.get("Actual Response").Values.Data;
-sig.A = logs.get("Disturbance A").Values.Data;
-sig.B = logs.get("Disturbance B").Values.Data;
-sig.e = logs.get("Error").Values.Data;
+sig.r  = logs.get("Desired Response").Values.Data;
+sig.d  = logs.get("Disturbance").Values.Data;
+sig.u  = logs.get("Controller Out").Values.Data;
+sig.y  = logs.get("Actual Response").Values.Data;
+sig.A  = logs.get("Disturbance A").Values.Data;
+sig.B  = logs.get("Disturbance B").Values.Data;
+sig.e  = logs.get("Error").Values.Data;
+sig.Kp_corr = logs.get("Kp_corr").Values.Data;
+sig.Ki_corr = logs.get("Ki_corr").Values.Data;
 end

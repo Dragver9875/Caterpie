dac9a48c2826eadb95632112f89fe56c18dd6d40
Piyush <pandapiyush086@gmail.com>
2026-04-07 01:56:58 +0530

    Update plot_episode_results.m

--- V1/matlab/eval/plot_episode_results.m
+++ V1/matlab/eval/plot_episode_results.m
@@ -2,28 +2,28 @@ function plot_episode_results(t, y, e, u, kpCorr, kiCorr, figTitle)
 figure('Name', figTitle);
 
 subplot(5,1,1);
-plot(t, y, 'LineWidth', 1.2); 
+plot(t, y, 'LineWidth', 1.2);
 grid on;
 ylabel('Response');
 title(figTitle);
 
 subplot(5,1,2);
-plot(t, e, 'LineWidth', 1.2); 
+plot(t, e, 'LineWidth', 1.2);
 grid on;
 ylabel('Error');
 
 subplot(5,1,3);
-plot(t, u, 'LineWidth', 1.2); 
+plot(t, u, 'LineWidth', 1.2);
 grid on;
 ylabel('Control');
 
 subplot(5,1,4);
-plot(t, kpCorr, 'LineWidth', 1.2); 
+plot(t, kpCorr, 'LineWidth', 1.2);
 grid on;
 ylabel('Kp\_corr');
 
 subplot(5,1,5);
-plot(t, kiCorr, 'LineWidth', 1.2); 
+plot(t, kiCorr, 'LineWidth', 1.2);
 grid on;
 ylabel('Ki\_corr');
 xlabel('Time (s)');
